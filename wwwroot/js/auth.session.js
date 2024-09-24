823b081e0b597f6c84423fa0f263ab669a6d2482
InacioCarvalhoOliveira <inacio.carvalho.oliveira02@gmail.com>
2024-09-24 15:53:42 -0300

    feat: melhoria js class

--- wwwroot/js/auth.session.js
+++ wwwroot/js/auth.session.js
@@ -1,35 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-  validateToken();
-});
-async function validateToken() {
-  setupLogoutButton();
-  const token = localStorage.getItem("authToken");
-  if (!token) {
-    alert("Unauthorized: Please log in first.");
-    window.location.href = "http://localhost:5039"; // Redirect to login page
-    return;
-  }
-
-  try {
-    const response = await fetch(
-      "http://localhost:8000/api/users/validateToken",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ token: token }),
-      }
-    );
-
-    if (response.ok) {
-      const data = await response.json();
-      console.log("Token is valid:", data);
-    } else {
-      throw new Error("Unauthorized");
+export class Auth {
+  static async validateToken() {
+    const token = localStorage.getItem("authToken");
+    if (!token) {
+      alert("Unauthorized: Please log in first.");
+      window.location.href = "http://localhost:5039"; // Redirect to login page
+      return;
     }
-  } catch (error) {
-    alert("Unauthorized: Please log in first.");
-    window.location.href = "http://localhost:5039"; // Redirect to login page
+
+    try {
+      const response = await fetch(
+        "http://localhost:8000/api/users/validateToken",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ token: token }),
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Token is valid:", data);
+        return token;
+      } else {
+        throw new Error("Unauthorized");
+      }
+    } catch (error) {
+      alert("Unauthorized: Please log in first.");
+      window.location.href = "http://localhost:5039"; // Redirect to login page
+    }
   }
 }
